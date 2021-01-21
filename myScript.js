b4b6eb883b7c5a47ca28d3497da412cccc220969
Wookshin <cotlsdnr1@gmail.com>
2021-01-21 23:57:20 +0900

    Delete myScript.js

--- myScript.js
+++
@@ -1,7 +0,0 @@
-for(var children of parent){
-	var child = children.firstElementChild;
-	while(child){
-			console.log(child.firstElementChild.firstElementChild.firstElementChild.firstElementChild.getAttribute('aria-label'));
-			child = child.nextElementSibling;
-	}
-}
