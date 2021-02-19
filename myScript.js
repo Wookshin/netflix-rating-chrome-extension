7b1c8aff3a94785d0f67ca9fb94565c6d0d983c8
Wookshin <cotlsdnr1@gmail.com>
2021-02-19 14:29:40 +0900

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,49 +1,76 @@
-//컨텐츠 페이지의 #user 입력된 값이 변경 되었을 '때'
-document.querySelector("#user").addEventListener("keyup", function () {
-  console.log("in Extension");
 
-  chrome.tabs.executeScript(
-    {
-      code: `(${inContent})()`,
-    },
-    ([result] = []) => {
-      if (!chrome.runtime.lastError) {
-        document.querySelector("#result").innerText += result.movie; // shown in devtools of the popup window
+document.querySelector("#chkRating").addEventListener("change", function () {
+  console.log("in Extension");
+  if(this.checked) {
+    chrome.tabs.executeScript(
+      {
+        code: `(${inContent})()`,
+      },
+      ([result] = []) => {
+        if (!chrome.runtime.lastError) {
+          //document.querySelector("#result").innerText += result.movie; // shown in devtools of the popup window
+        }
       }
-    }
-  );
+    );
+  }
+  else
+  {
+    
+  }
 });
 
 function inContent() {
-  const userAction = (moive) => {
-    fetch("http://localhost:3000/api/movie/" + movie, {
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-      },
-    }).then((res)=>res.json())
-    .then((myJson)=>console.log(JSON.stringify(myJson)));
-  };
+  const fn_SetRating = async (name, target) => {
+    let response = await fetch('http://localhost:3000/movie/'+name);
+    let myJson = await response.json();
+    let span;
+    let rating = myJson["rating"];
 
-  var children = document.querySelectorAll(".title-card-container");
+    console.log(name, rating, target);
+    span = fn_GetSpan(rating);
+    target.appendChild(span);
+  }
 
-  var movie = "";
+  const fn_GetSpan = (rating) => {
+    let span = document.createElement("span");
+    span.textContent = rating;
+    span.style.height="3rem";
+    span.style.width = "3rem";
+    span.style.backgroundColor = "rgb(62, 226, 57)";
+    span.style.borderRadius = "50%";
+    span.style.color = 'white';
+    span.style.fontSize = '1.8rem';
+    span.style.display = 'flex'; 
+    span.style.justifyContent = 'center';
+    span.style.top = '0';
+    span.style.right = '0';
+    span.style.position = 'absolute';
+
+    return span;
+  }
+
+  let children = document.querySelectorAll(".title-card-container");
+  let movieName;
+  let target;
+
   for (var child of children) {
     if (!child) continue;
 
-    movie = child.firstElementChild.firstElementChild.firstElementChild
-      .getAttribute("aria-label")
-      .trim();
-    console.log(movie);
-    userAction(movie);
+    target = child?.firstElementChild?.firstElementChild?.firstElementChild?.firstElementChild
+    if(target != undefined){
+      movieName = target?.firstElementChild?.nextElementSibling?.firstElementChild?.textContent?.trim();
 
-    break;
+      if(movieName != undefined){
+        fn_SetRating(movieName, target);
+      }
+    }
   }
-
-  // console.log(movies);
 
   return {
     success: true,
-    movie: movie,
+    movie: movieName,
+    //rating: rating
   };
 }
+
+
