05b6ccedd57b040ba8af4fd768d06bbd555f7c90
Wookshin <cotlsdnr1@gmail.com>
2021-03-11 22:34:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,19 @@
 const express = require("express");
 var cors = require("cors");
-const fetch = require("node-fetch");
+var bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 
 app.use(cors());
+app.use(bodyParser.json())
 
-const getRating = (moive, f) => {
-  fetch(
-    "https://openapi.naver.com/v1/search/movie.json?query=" + encodeURI(moive),
-    {
-      headers: {
-        "X-Naver-Client-Id": "QTDpPsZMRYtMqBUdl6jw",
-        "X-Naver-Client-Secret": "c6JHcn2LPr",
-      },
-    }
-  )
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(json);
-      f(json.items[0]["userRating"]);
-    });
-};
+// 라우터 객체
+var indexRouter = require('./routes/index');
+var apiRouter = require('./routes/api');
 
-
-app.get("/api/moive/:name", (req, res) => {
-  console.log(req.params.name);
-  getRating(req.params.name, function (rating) {
-    res.json({rating: rating});
-  });
-});
-
-app.get("/api/moive", (req, res) => {
-  console.log("movie!");
-  res.json({msg : "movie"});
-});
-
-app.get("/", (req, res) => {
-  console.log("hello world!");
-  res.json({msg : "Hello World"});
-});
+// 라우팅
+app.use('/', indexRouter);
+app.use('/movie', apiRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
