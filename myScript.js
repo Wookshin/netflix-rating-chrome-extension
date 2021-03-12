2e7ea5cf15bd1f92908a3781b84626203862a3b7
Wookshin <cotlsdnr1@gmail.com>
2021-03-12 16:06:49 +0900

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -19,12 +19,18 @@ document.querySelector("#chkRating").addEventListener("change", function () {
 });
 
 function inContent() {
-  const fn_SetRating = async (names, targets) => {
-    let data = {
-      names : names
+  const fn_SetRating = async (newMovies) => {
+    console.log("new",newMovies);
+    let names = [];
+    for(let movie of newMovies){
+      names.push(movie.name);
     }
 
-    let response = await fetch('http://localhost:3000/movie/', {
+    let data = {names : names};
+
+    console.log("fn_SetRating");
+
+    let response = await fetch('http://localhost:3000/api/', {
       method: 'POST', // or 'PUT'
       body: JSON.stringify(data), // data can be `string` or {object}!
       headers:{
@@ -33,13 +39,17 @@ function inContent() {
     });
     let myJson = await response.json();
 
-    console.log(myJson);
+    console.log("myjson", myJson);
 
     let span;
 
-    for(let i=0; i<myJson["list"].length; i++){    
-      span = fn_GetSpan(myJson["list"][i]["rating"]);
-      targets[i].appendChild(span);
+    for(let rtnVal of myJson["list"]){    
+      for(let movie of newMovies){
+        if(movie.name === rtnVal.name){
+          span = fn_GetSpan(rtnVal["rating"]);
+          movie.target.appendChild(span);
+        }
+      }
     }
 
     //console.log(name, rating, target);
@@ -63,13 +73,28 @@ function inContent() {
     return span;
   }
 
-  console.log("inContent");
+  console.log("Check existing new movie target.");
 
   let children = document.querySelectorAll(".title-card-container");
+
+  if(document.movieCount == undefined){
+    document.movieCount = 0;
+  }
+
+  if(document.movieCount == children.length){
+    return;
+  }
+
   let movieName;
-  let movieNames = [];
+  let newMovieNames = [];
+  let newMovies = [];
   let target;
   let targets = [];
+
+  if(document.oldMovieNames == undefined){
+    console.log("??");
+    document.oldMovieNames = [];
+  }
 
   for (var child of children) {
     if (!child) continue;
@@ -78,18 +103,39 @@ function inContent() {
     if(target != undefined){
       movieName = target?.firstElementChild?.nextElementSibling?.firstElementChild?.textContent?.trim();
 
-      if(movieName != undefined){
+      if(movieName != undefined && document.oldMovieNames.includes(movieName) == false){
+        newMovies.push({name:movieName, target:target});
         targets.push(target);
-        movieNames.push(movieName);
+        newMovieNames.push(movieName);
+        document.oldMovieNames.push(movieName);
       }
     }
   }
 
-  fn_SetRating(movieNames, targets);
+  console.log(document.movieList);
+  //fn_SetRating(newMovieNames, targets);
+  
+  // name 기준으로 정렬
+  // newMovies.sort(function(a, b) {
+  //   var nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  //   var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  //   if (nameA < nameB) {
+  //     return -1;
+  //   }
+  //   if (nameA > nameB) {
+  //     return 1;
+  //   }
+  //   // 이름이 같을 경우
+  //   return 0;
+  // });
+
+  fn_SetRating(newMovies);
+
+  document.movieCount = children.length;
 
   return {
     success: true,
-    movie: movieName,
+    count: children.length,
     //rating: rating
   };
 }
