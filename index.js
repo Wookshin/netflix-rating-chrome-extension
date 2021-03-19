b796f32fa97948323e616e431f5c33353f6ffcb2
Wookshin <cotlsdnr1@gmail.com>
2021-03-19 15:42:24 +0900

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get("/", (req, res) => {
-  console.log("hello world!");
-  res.json({msg : "Hello World"});
-});
-
-module.exports = router;
